f6d4ab13a09830882106434b976da4aea495f9f7
SNesht <nestorcarmo@gmai.com>
2026-06-08 13:46:38 -0300

    coral melhora

--- main.cpp
+++ main.cpp
@@ -168,10 +168,8 @@ void desenhaRocha(float x, float y, float w, float h) {
 // Um coral (varias elipses em volta de um centro).
 void desenhaCoral(float x, float y, float r, float rg, float rb) {
     glColor3f(r, rg, rb);
-    for (int i = 0; i < 3; i++) {
-        float a = 2.0f * M_PI * i / 6;
-        elipse(x + 3 * (i - 1), y, 4, 10);
-    }
+    elipse(x - 3, y, 3, 12);
+    elipse(x + 3, y, 3, 12);
     circulo(x, y, 8);
 }
 
